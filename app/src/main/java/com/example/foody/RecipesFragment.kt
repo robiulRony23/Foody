cb1e3d8d3fed23181cb630c02bc2612d8f137418
robiulRony23 <robiulislamrony5@gmail.com>
2023-10-29 17:22:21 +0600

    add shimmer recyclerView in recipes fragment

--- app/src/main/java/com/example/foody/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.todkars.shimmer.ShimmerRecyclerView
 
 class RecipesFragment : Fragment() {
 
@@ -13,6 +14,8 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        view.findViewById<ShimmerRecyclerView>(R.id.srv_recipes).showShimmer();
+        return view;
     }
 }
